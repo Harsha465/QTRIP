281e71974eb36459acbc5516a61c19415f1587c0
CrioUser <crio-user@criodo.com>
2024-02-01 00:39:44 +0530

    ME_QTRIP_QA_V2_MODULE_CREATE_TESTNG1

--- app/src/test/java/qtriptest/DP.java
+++ app/src/test/java/qtriptest/DP.java
@@ -1,45 +1,45 @@
 package qtriptest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 
 public class DP {
-    // TODO: use correct annotation to connect the Data Provider with your Test Cases
-    
-    public Object[][] dpMethod(Method m) throws IOException {
-        int rowIndex = 0;
-        int cellIndex = 0;
-        List<List> outputList = new ArrayList<List>();
 
-        FileInputStream excelFile = new FileInputStream(new File(
-                "<absolute-path-to-xlsx-file>"));
-        Workbook workbook = new XSSFWorkbook(excelFile);
-        Sheet selectedSheet = workbook.getSheet(m.getName());
-        Iterator<Row> iterator = selectedSheet.iterator();
-        while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
-            List<String> innerList = new ArrayList<String>();
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                if (rowIndex > 0 && cellIndex > 0) {
-                    if (cell.getCellType() == CellType.STRING) {
-                        innerList.add(cell.getStringCellValue());
-                    } else if (cell.getCellType() == CellType.NUMERIC) {
-                        innerList.add(String.valueOf(cell.getNumericCellValue()));
+    @DataProvider(name="DatasetsforQTrip")
+    public Object [][] DatasetsforQTrip(Method m) throws IOException{
+        try {
+            File flObj = new File("/home/crio-user/workspace/kondaharsha17-ME_QTRIP_QA_V2/app/src/test/resources/DatasetsforQTrip.xlsx");
+            // /home/crio-user/workspace/kondaharsha17-ME_QTRIP_QA_V2/app/src/test/resources/DatasetsforQTrip.xlsx
+            FileInputStream fileInputObj = new FileInputStream(flObj);
+            try (XSSFWorkbook wbObj = new XSSFWorkbook(fileInputObj)) {
+                switch (m.getName()) {
+   
+                  case "TestCase01":
+                  XSSFSheet sheet1 = wbObj.getSheet("TestCase01");
+                  int TC1_rowCount = sheet1.getPhysicalNumberOfRows();
+                  int TC1_colCount = sheet1.getRow(0).getLastCellNum();
+                  
+                  Object[][] testData1 = new Object[TC1_rowCount-1][TC1_colCount-1];
+                  
+                  for (int i = 1; i < TC1_rowCount; i++) {
+                    for(int j = 1; j < TC1_colCount; j++ ){
+                      String value = sheet1.getRow(i).getCell(j).toString();
+                      testData1[i-1][j-1] = value;
                     }
+                  }
+
+                  return testData1;
                 }
-                cellIndex = cellIndex + 1;
             }
-            rowIndex = rowIndex + 1;
-            cellIndex = 0;
-            if (innerList.size() > 0)
-                outputList.add(innerList);
-
+    }catch(Exception e)
+            {
+            e.printStackTrace();
+          }
+        return null;
         }
-
-        excelFile.close();
-
-        String[][] stringArray = outputList.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
-        return stringArray;
-
     }
-}
